eae0f4a91b0fd542f57a015200fb21fce0694b8a
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2013-09-06 16:36:08 +0200

    Implement rename-id for renaming identifiers.

---
+++ rename-id.bat
@@ -0,0 +1,28 @@
+@echo off
+
+setlocal
+
+if "%1"=="" goto USAGE
+set FILE=%1
+shift
+
+if "%1"=="" goto USAGE
+set PATTERN=%1
+shift
+
+if "%1"=="" goto USAGE
+set TEMPLATE=%1
+shift
+
+gfind -iname "%FILE%" | xargs sed -i "s/%PATTERN%$/%TEMPLATE%/g"
+gfind -iname "%FILE%" | xargs sed -i "s/%PATTERN% /%TEMPLATE% /g"
+
+goto END
+
+:USAGE
+echo rename-id FILE PATTERN TEMPLATE
+echo.
+echo DON'T USE QUOTES!
+goto END
+
+:END
