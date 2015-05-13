0ab38f4e8454ea32e885242ebbb9c3b10c543641
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2015-05-13 09:02:32 +0200

    Automate doskey macros.

---
+++ dot-cmd.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem consider to register the full path to this file
+rem
+rem HKEY_CURRENT_USER\Software\Microsoft\Command Processor
+rem autorun : REG_SZ = "...\dot-cmd.bat"
+
+doskey ls=ls --color=auto $*
+doskey ll=ls -l --color=auto $*
+echo Aliases installed from %~f0.
